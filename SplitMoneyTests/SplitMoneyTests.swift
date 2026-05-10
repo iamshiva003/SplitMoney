55f8671f6446b56eca287bca54caa84a61c4d6ca
Shivakumar Patil <shivu9887@gmail.com>
2026-05-10 11:57:08 +0530

    Initial Commit

---
+++ SplitMoneyTests/SplitMoneyTests.swift
@@ -0,0 +1,19 @@
+//
+//  SplitMoneyTests.swift
+//  SplitMoneyTests
+//
+//  Created by Shivakumar Patil on 10/05/26.
+//
+
+import Testing
+@testable import SplitMoney
+
+struct SplitMoneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
